871807a4956e01c74698cd7f8037f1f283fe347c
dae54 <amethysternest@gmail.com>
2021-03-24 03:47:51 +0300

    add more routes to component

--- src/Components/routes.jsx
+++ src/Components/routes.jsx
@@ -2,8 +2,11 @@ import Login from '../Pages/Authentication/Login'
 
 import {
     DesktopOutlined,
+    UserOutlined,
+    FundProjectionScreenOutlined
 } from '@ant-design/icons';
 import Dashboard from '../Pages/Dashboard';
+import { Users } from '../Pages/Manufacturer';
 
 const routes = [
     {
@@ -13,7 +16,63 @@ const routes = [
         Icon: DesktopOutlined,
         comments: 'Dashboard endpoint',
         category: 1,
-        sidebar: true
+        sidebar: true,
+        key: 1
+    },
+    {
+        name: 'Manufacturers',
+        Icon: FundProjectionScreenOutlined,
+        comments: 'Dashboard endpoint',
+        category: 1,
+        sidebar: true,
+        key: 2,
+        submenu: [
+            {
+                name: 'Users',
+                url: '/manufacturers/users',
+                icon: UserOutlined,
+                component: Users,
+                key: 21
+            },
+            {
+                name: 'Products',
+                url: '/manufacturers/products',
+                icon: UserOutlined,
+                component: 'Users',
+                key: 22
+            },
+            // {
+            //     name: 'Users',
+            //     url: '/manufacturers/users',
+            //     icon: UserOutlined,
+            //     component: 'Users',
+            //     key: 23
+            // },
+        ]
+    },
+    {
+        name: 'Quality Controllers',
+        Icon: FundProjectionScreenOutlined,
+        comments: 'Dashboard endpoint',
+        category: 1,
+        sidebar: true,
+        key: 3,
+        submenu: [
+            {
+                name: 'Users',
+                url: '/manufacturers/users',
+                icon: UserOutlined,
+                component: Users,
+                key: 31
+            },
+            {
+                name: 'Products',
+                url: '/manufacturers/products',
+                icon: UserOutlined,
+                component: 'Users',
+                key: 32
+            },
+        ]
     },
     {
         name: 'login',
