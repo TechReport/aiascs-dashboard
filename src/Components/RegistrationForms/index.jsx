2837ad1feff1e5084d888f3c940632ec85a30939
dae54 <amethysternest@gmail.com>
2021-03-24 03:50:57 +0300

    create retistration forms

---
+++ src/Components/RegistrationForms/index.jsx
@@ -0,0 +1,9 @@
+import AgentRegistrationForm from './AgentRegistrationForm'
+import ManufacturingCompanyRegistrationForm from './ManufacturingCompanyRegistrationForm'
+import ProductRegistrationForm from './ProductRegistrationForm'
+
+export {
+    ManufacturingCompanyRegistrationForm,
+    AgentRegistrationForm,
+    ProductRegistrationForm
+}
