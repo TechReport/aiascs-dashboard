4adb39ac71d6dd058b239c9cf9f1d4ccbb021ceb
dae54 <amethysternest@gmail.com>
2021-03-24 03:48:29 +0300

    add breadcrumb compnent

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -5,13 +5,20 @@ import { Fragment } from "react";
 import {
     UserOutlined
 } from '@ant-design/icons';
+import { Breadcrumb } from "antd";
 
 export default function Navbar() {
     return (
         <Fragment>
             <Header className="site-layout-background bg-light" >
                 <div className='row bg-inf mr-n5 d-flex justify-content-between'>
-                    <div className="bg-dange">d</div>
+                    <div className="bg-dange py-2">
+                        <Breadcrumb style={{ margin: '16px 0' }}>
+                            <Breadcrumb.Item>Home</Breadcrumb.Item>
+                            <Breadcrumb.Item>List</Breadcrumb.Item>
+                            <Breadcrumb.Item>App</Breadcrumb.Item>
+                        </Breadcrumb>
+                    </div>
                     <div className="bg-dange pr-2">
                         <span className='bg-white rounded-pill px-2 py-1'>
                             Daniel ernest
