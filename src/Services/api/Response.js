63f25ed4be9081ab00ca13dea0c999465b3adc5c
dae54 <amethysternest@gmail.com>
2021-04-01 08:13:31 +0300

    customize error handler login

--- src/Services/api/Response.js
+++ src/Services/api/Response.js
@@ -1,28 +1,52 @@
 // import { EventEmitter } from 'events'
-import eventEmitter from '../EventEmitter'
+// import eventEmitter from '../EventEmitter'
 
 export function handleResponse(response) {
-    console.log(response)
+    // console.log(response)
     if (response.results) {
+        console.log('UNEXPECTED')
         return response.results;
-    }
-
-    if (response.data) {
+    } else if (response.data) {
+        // console.log('return here')
+        // console.log(response.data)
         return response.data;
+    } else {
+        console.log('SIELEW')
+        return response;
     }
-
-    return response;
 }
 
 export function handleError(error) {
-    // if (error.data) {
-    //     throw error.data;
-    // }
+    // console.log(error.response)
     if (error.message === 'Network Error') {
-        console.log('weve network error')
+        // eslint-disable-next-line
         throw { status: 'error', response: [{ message: 'Network Error', data: { dev_error: error, descriptions: 'Something is temporarily wrong with your network connection. Please make sure you are connected to the internet and then try again' } }] }
     } else {
-        throw { status: 'error', response: [{ message: error.response.message, data: { dev_error: error.response.errorObj, descriptions: 'Something went wrong. Please retry after sometimes' } }] }
+        if (error.response.data.category === 'invalidInput') {
+            // eslint-disable-next-line
+            throw {
+                message: error.response.data.message,
+                descriptions: 'Please correct the entry and try again',
+                debug: {
+                    dev_error: error.response.data.developerMessage,
+                    stack: error.response.data.stack
+                }
+            }
+        } else if (error.response.data.category === 'unauthorized') {
+            // eslint-disable-next-line
+            throw {
+                message: error.response.data.message,
+                descriptions: 'Please login to access the resource',
+                debug: {
+                    dev_error: error.response.data.developerMessage,
+                    stack: error.response.data.stack
+                }
+            }
+        }
+        else {
+            // eslint-disable-next-line
+            throw { status: 'error', response: { message: error.response.data.message, data: { dev_error: error.response.data.errorObj, descriptions: 'Something went wrong. Please retry after sometimes' } } }
+        }
         // throw error;
     }
 }
