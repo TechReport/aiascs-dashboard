ef95b3e4c57c2058679d9b6c4f7fccbb7550a4a6
dae54 <amethysternest@gmail.com>
2021-04-01 07:56:06 +0300

    set axios default configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import { BrowserRouter } from 'react-router-dom';
 import withSplashScreen from './Components/withSplashScreen'
 import MainLayout from './Pages/Layout';
+import axios from 'axios'
 import 'antd/dist/antd.css';
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css'
+
+const baseURL = 'http://localhost:5400/'
+
+
+axios.defaults.headers.common['Authorization'] =
+  localStorage.getItem('token') ? `Bearer ${localStorage.getItem('token')}` : 'undefined'
+
+axios.defaults.timeout = 20000
+axios.defaults.baseURL = baseURL
+
 
 function App() {
   return (
@@ -12,3 +24,4 @@ function App() {
 }
 
 export default withSplashScreen(App);
+// export default App; 
