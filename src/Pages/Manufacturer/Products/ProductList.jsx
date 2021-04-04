8daf65f3741250633c00c0bfafe28225a225bee7
dae54 <amethysternest@gmail.com>
2021-04-04 03:58:27 +0300

    feat: add createdat column in product table

--- src/Pages/Manufacturer/Products/ProductList.jsx
+++ src/Pages/Manufacturer/Products/ProductList.jsx
@@ -1,8 +1,14 @@
-import { Skeleton } from 'antd'
+import { Button, Skeleton, Tag } from 'antd'
 import { useEffect, useState } from 'react'
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
 import { productAPI } from './productAPI'
 import eventEmitter from '../../../Services/EventEmitter'
+import {
+    DeleteOutlined,
+    EditOutlined,
+    EyeOutlined,
+} from '@ant-design/icons';
+import moment from 'moment'
 
 export default function ProductList() {
     const [products, setProducts] = useState({ loading: true, data: [] })
@@ -31,10 +37,22 @@ export default function ProductList() {
             <Skeleton active />
             :
             <BootstrapTable data={products.data} pagination search scrollTop='Top' striped hover searchPlaceholder='Search products' trClassName='bg-inf border' >
-                <TableHeaderColumn dataField='Name' filterFormatted dataFormat={(cell, row) => `${row.firstName} ${row.lastName}`} isKey>Name</TableHeaderColumn>
-                <TableHeaderColumn dataField='Batch'>Batch</TableHeaderColumn>
-                <TableHeaderColumn dataField='phoneNumber'>QR code</TableHeaderColumn>
-                <TableHeaderColumn dataField='expiry'>Expiry</TableHeaderColumn>
+                <TableHeaderColumn dataField='name' isKey width='150'>Name</TableHeaderColumn>
+                <TableHeaderColumn dataField='qrcode' dataFormat={cell => cell.productToken}>Token</TableHeaderColumn>
+                <TableHeaderColumn dataField='isRevoked' width='90' dataFormat={(cell) => cell ? <Tag color='magenta'>True</Tag> : <Tag color='cyan'>False</Tag>}>Revoked ?</TableHeaderColumn>
+                <TableHeaderColumn dataField='createdAt' dataFormat={(cell) => moment(cell).format('DD-MM-YYYY')} width='100' >Registered At</TableHeaderColumn>
+                <TableHeaderColumn dataField='expiry' dataFormat={(cell) => moment(cell).format('DD-MM-YYYY')} width='100' >Expiry</TableHeaderColumn>
+                <TableHeaderColumn dataField='' width='100' dataFormat={actionMenu}>Actions</TableHeaderColumn>
             </BootstrapTable>
+    )
+}
+
+function actionMenu() {
+    return (
+        <>
+            <Button size='small' shape='circle' type='text'><DeleteOutlined className='text-danger' /></Button>
+            <Button size='small' shape='circle' type='text'><EditOutlined className='text-primary' /></Button>
+            <Button size='small' shape='circle' type='text'><EyeOutlined className='text-dark' /></Button>
+        </>
     )
 }
