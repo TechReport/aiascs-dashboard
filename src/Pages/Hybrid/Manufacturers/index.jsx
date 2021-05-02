07127fb7775bea55e0176c73171d1b1ee85ca2f4
dae54 <amethysternest@gmail.com>
2021-05-02 12:58:09 +0300

    add products profile

--- src/Pages/Hybrid/Manufacturers/index.jsx
+++ src/Pages/Hybrid/Manufacturers/index.jsx
@@ -2,5 +2,7 @@ import Manufacturers from './Manufacturers'
 import ManCompanyProfile from './CompanyProfile'
 import UnregisteredProducts from './Products/UnregisteredProducts'
 import UnregisteredProductProfile from './Products/UnregisteredProducts/UnregisteredProductProfile'
+import Product from './Products/Product'
+import Products from './Products'
 
-export { Manufacturers, ManCompanyProfile, UnregisteredProducts, UnregisteredProductProfile }
+export { Manufacturers, ManCompanyProfile, UnregisteredProducts, UnregisteredProductProfile, Product, Products }
