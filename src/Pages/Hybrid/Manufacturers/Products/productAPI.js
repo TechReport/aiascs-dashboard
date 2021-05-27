c4b9b7b69a1f1a455c6bed4e19fbaf6b53bc68c0
dae54 <amethysternest@gmail.com>
2021-05-27 18:40:29 +0300

    revoke product

--- src/Pages/Hybrid/Manufacturers/Products/productAPI.js
+++ src/Pages/Hybrid/Manufacturers/Products/productAPI.js
@@ -1,19 +1,26 @@
 // Task API
-import { ApiCore } from '../../../../Services/api/Core'
-
+import axios from "axios";
+import { ApiCore } from "../../../../Services/api/Core";
+import { handleError, handleResponse } from "../../../../Services/api/Response";
 
 // plural and single may be used for message logic if needed in the ApiCore class.
 
 const productAPI = new ApiCore({
-    getAll: true,
+  getAll: true,
 
-    // getSingle: true,
-    post: true,
-    deleteOne: true
+  // getSingle: true,
+  post: true,
+  deleteOne: true,
 });
 
-// apiTasks.massUpdate = () => {
-//     // Add custom api call logic here
-// }
-
+productAPI.activity = async (productID) => {
+  return await axios
+    .get(`products/activity/${productID}`)
+    .then((res) => {
+      return handleResponse(res);
+    })
+    .catch((err) => {
+      return handleError(err);
+    });
+};
 export { productAPI };
