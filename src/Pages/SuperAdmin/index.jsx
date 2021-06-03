f2689fb4691ff2c5a6d2f1c90bf75f73fe82573c
dae54 <amethysternest@gmail.com>
2021-06-03 13:53:13 +0300

    refactor unwanted

--- src/Pages/SuperAdmin/index.jsx
+++
@@ -1,6 +0,0 @@
-import Manufacturers from './Manufacturers'
-import ManCompanyProfile from './Manufacturers/ManCompanyProfile'
-
-import QualityControllers from './QualityControllers'
-
-export { Manufacturers, ManCompanyProfile, QualityControllers }
