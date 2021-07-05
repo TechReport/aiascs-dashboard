04fa5ae8b58e55d72b956c15d1fd251241b1f0ea
dae54 <amethysternest@gmail.com>
2021-07-05 14:13:23 +0300

    cmpany profle

--- src/Pages/Reports/Hybrid/ProductCompany.jsx
+++ src/Pages/Reports/Hybrid/ProductCompany.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react'
 import moment from 'moment'
-import UdsmLogo from '../../../Assets/logo_ud.png'
+// import UdsmLogo from '../../../Assets/logo_ud.png'
+import CoaTZ from '../../../Assets/logos/coatz.jpg'
+import { UserOutlined } from '@ant-design/icons'
 
 import { reportsAPI } from '../reportsApi'
 import { ConfigurationContext } from './configurations.context'
@@ -8,12 +10,17 @@ import { Skeleton } from 'antd'
 import Level1 from './Products/Level1';
 import Level2 from './Products/Level2'
 import Level3 from './Products/Level3'
+// import Avatar from 'antd/lib/avatar/avatar'
+// import { AuthContext } from '../../../Context/AuthContext'
 
 export default function ProductCompany() {
     const [reports, setReports] = useState({ loading: false, data: [] })
     const { state } = useContext(ConfigurationContext)
     const [levelSelect, setLevelSelect] = useState({ index: 0, data: {} })
-
+    const [companyDetails] = useState(JSON.parse(sessionStorage.getItem('user')).companyId)
+    console.log(companyDetails)
+    // const currentUser = useContext(AuthContext).state.currentUser
+    // console.log(currentUser)
     // const titles = [
     //     'Product Registration Report as per COmpany for the year ending 31/12/2020.',
     //     'Product Registration Report from ',
@@ -51,6 +58,10 @@ export default function ProductCompany() {
         // eslint-disable-next-line
     }, [])
 
+    /**
+     * qc
+     * admin
+     */
     return (
         reports.loading ?
             <Skeleton active round avatar />
@@ -58,15 +69,20 @@ export default function ProductCompany() {
             <div className='container-fluid p-0'>
                 <div className="card">
                     <div className="card-body border" id='reportContents'>
-                        <div className="titlebar d-flex justify-content-between">
-                            <img src={UdsmLogo} className='img' alt='company logo' width='100px' />
-                            <div className="descriptions text-right">
-                                <span className='h6'>Tanzania Bureau of Standards (TBS) </span><br />
-                                <span className='h'>P.O BOX 2956</span> <br />
-                                <span className='h'>Telephone: +255 26 2520310.</span><br />
-                                <span className='h'>Fax: +255 26 2520310.</span> <br />
+                        <div className="titlebar text-center">
+                            {/* {companyDetails.logo ? */}
+                            <img src={CoaTZ} className='img-fluid' alt='company logo' width='100px' />
+                            <h6>The United Republic of Tanzania</h6>
+                            <h5 className=''>Agro Input Assurance Supply Chain System</h5>
+                            {/* // : */}
+                            {/* // <Avatar shape="square" size={100} icon={<UserOutlined />} /> */}
+                            {/* // } */}
+                            {/* <div className="descriptions text-right">
+                                <span className='h6'>{companyDetails.name} </span><br />
+                                <span className='h'>{`P.O BOX ${companyDetails.postalBox.boxNumber}, ${companyDetails.postalBox.boxLocation.region}`}</span> <br />
+                                <span className='h'>Telephone: {companyDetails.phonenumber}.</span><br />
                                 <span className='h'>{moment(Date.now()).format('ddd DD, MMM YYYY')}</span>
-                            </div>
+                            </div> */}
                         </div>
 
                         <div className="report-body">
